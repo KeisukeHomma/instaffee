ad6d000dd6c00a1239b12ef878f8abb74e35887f
Homma <ab13891012.gmail.com>
2016-09-30 17:22:12 +0900

    dive3 start

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,21 +10,33 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(posts_params)
-    redirect_to posts_path
+    @post = Post.new(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render action: 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    @post.update(posts_params)
+    redirect_to posts_path
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を消去しました！"
   end
 
   private
     def posts_params
       params.require(:post).permit(:title)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
