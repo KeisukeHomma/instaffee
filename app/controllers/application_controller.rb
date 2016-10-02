15d026ae79e6621afcdb8d87b37824978ceedaf4
Homma <ab13891012.gmail.com>
2016-10-02 17:36:23 +0900

    user login test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
   #変数PERMISSIBLE_ATTRIBUTESに配列[:name]を代入
   PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url, :alert => exception.message
-  end
+  #rescue_from CanCan::AccessDenied do |exception|
+  #  redirect_to main_app.root_url, :alert => exception.message
+  #end
 
   protected
 
